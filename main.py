169b71d610d6ec70decc092b917e31a7bf405c2b
Philip <ppoliziani@hotmail.com>
2021-09-28 18:21:05 +0100

    removed random print statements

--- main.py
+++ main.py
@@ -49,28 +49,28 @@ def k_nearest_neighbours():
 def nearest_neighbours(coordinates, test_sample):
 
     # calculate distance from test_data to all coordinates
-    print("\n= Using NN algorithm =\n")
+    # print("\n= Using NN algorithm =\n")
 
     distances = np.zeros([10, 1])
-    print(distances)
+    # print(distances)
 
     graph_coords = np.delete(coordinates, -1, 1)
 
     for x in range(0, 10):
         distance = math.sqrt((graph_coords[x][0] - test_sample[0])**2 + (graph_coords[x][1] - test_sample[1])**2)
         distances[x] = distance
-        print(distance)
+        # print(distance)
 
     # find index of shortest distance in distances then return label of same index in coordinates.
     # some sort of search
-    print("MIN: " + str(np.amin(distances)))
+    # print("MIN: " + str(np.amin(distances)))
     # change this to take k values
     index = np.where(distances == np.amin(distances))
     index = index[0][0]
 
     # then look at labels of k values and decide prediction
     prediction = coordinates[index][2]
-    print(coordinates[index])
+    # print(coordinates[index])
     print("\n= NN PREDICTION =\n")
     print(prediction)
 
@@ -96,13 +96,12 @@ def generate_samples():
             label = 1
 
         coordinate = [x_val, y_val, label]
-
         coordinates[x] = coordinate
 
-    print(coordinates)
+    # print(coordinates)
 
     test_sample = [random.randint(-3, 10), random.randint(-3, 10), 0]
-    print("Test: " + str(test_sample))
+    # print("Test: " + str(test_sample))
 
     nearest_neighbours(coordinates, test_sample)
 
